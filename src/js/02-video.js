9b672419e0e943fd818ece0cb5711814a5170e9c
Ihor Statsenko <statsenkoin@gmail.com>
2022-12-13 23:54:00 +0200

    02-video player setup

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Player(iframe);
+let videoPlayerCurrentTime = 0;
+
+iframePlayer.on('play', function () {
+  console.log('played the video!');
+  console.log('videoPlayerCurrentTime :>> ', videoPlayerCurrentTime);
+});
+
+iframePlayer.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+// ========================================================
+
+iframePlayer.on('timeupdate', function (e) {
+  //   console.log('seconds: ', e.seconds);
+  videoPlayerCurrentTime = e.seconds;
+});
