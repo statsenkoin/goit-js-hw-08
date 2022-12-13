9d0ea459fc4e3349544a4a8abc7a31793d9da5ee
Ihor Statsenko <statsenkoin@gmail.com>
2022-12-13 23:09:32 +0200

    reset src + 01-gallery completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,37 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// ==========================================================
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// =============================================================
+
+const gallery = document.querySelector('.gallery');
+
+createMarkUp();
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  close: true,
+});
+
+function createMarkUp() {
+  const markup = galleryItems.reduce(
+    (acc, { preview, original, description }) => {
+      acc += `<a class="gallery__item" href="${original}">
+                <img
+                  class="gallery__image"
+                  src="${preview}"
+                  alt="${description}"
+                />
+              </a>`;
+      return acc;
+    },
+    ''
+  );
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
