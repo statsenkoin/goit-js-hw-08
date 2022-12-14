81e84135eb4016ccfd1e8b21f56260366a493fd1
Ihor Statsenko <statsenkoin@gmail.com>
2022-12-14 22:39:02 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+const _throttle = require('lodash.throttle');
 
+let feedbackFormState = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', _throttle(onInputChange, 500));
+form.addEventListener('submit', onSubmit);
+
+onFormUpdate();
+
+function onFormUpdate() {
+  feedbackFormState = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  form.elements.email.value = feedbackFormState.email;
+  form.elements.message.value = feedbackFormState.message;
+
+  //   console.log('onFormUpdate->feedbackFormState :>> ', feedbackFormState);
+}
+
+function onInputChange() {
+  feedbackFormState.email = form.elements.email.value;
+  feedbackFormState.message = form.elements.message.value;
+
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(feedbackFormState)
+  );
+  //   console.log('onInputChange->feedbackFormState :>> ', feedbackFormState);
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  console.log('savedFormState :>> ', feedbackFormState);
+  feedbackFormState.email = '';
+  feedbackFormState.message = '';
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(feedbackFormState)
+  );
+  e.currentTarget.reset();
+}
