83a507919fc28d7fd443d794d2c55be8033ff394
Ihor Statsenko <statsenkoin@gmail.com>
2022-12-14 18:05:20 +0200

    02-video completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,21 +1,26 @@
 import Player from '@vimeo/player';
+const _throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
 const iframePlayer = new Player(iframe);
-let videoPlayerCurrentTime = 0;
 
-iframePlayer.on('play', function () {
-  console.log('played the video!');
-  console.log('videoPlayerCurrentTime :>> ', videoPlayerCurrentTime);
-});
+// ===== без throttle ===================================================
+// iframePlayer.on('timeupdate', function (e) {
+//   localStorage.setItem('videoplayer-current-time', e.seconds.toString());
+// });
 
-iframePlayer.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+iframePlayer.on(
+  'timeupdate',
+  _throttle(function (e) {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      Math.floor(e.seconds).toString()
+    );
+    console.log(
+      'videoplayer-current-time (seconds) >> ',
+      localStorage.getItem('videoplayer-current-time')
+    );
+  }, 1000)
+);
 
-// ========================================================
-
-iframePlayer.on('timeupdate', function (e) {
-  //   console.log('seconds: ', e.seconds);
-  videoPlayerCurrentTime = e.seconds;
-});
+iframePlayer.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
