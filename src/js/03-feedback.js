06088fc2840315dd405c072782379f7534428417
Ihor Statsenko <statsenkoin@gmail.com>
2022-12-15 13:36:29 +0200

    fix initial form setup

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,23 +1,30 @@
-const _throttle = require('lodash.throttle');
+// const _throttle = require('lodash.throttle');
+import _throttle from 'lodash.throttle';
 
 let feedbackFormState = {
   email: '',
   message: '',
 };
+if (!localStorage.getItem('feedback-form-state')) {
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(feedbackFormState)
+  );
+}
 
 const form = document.querySelector('.feedback-form');
 form.addEventListener('input', _throttle(onInputChange, 500));
 form.addEventListener('submit', onSubmit);
 
 onFormUpdate();
-
+// fill in form fields if any data in localStorage avialable from previous session
 function onFormUpdate() {
   feedbackFormState = JSON.parse(localStorage.getItem('feedback-form-state'));
 
   form.elements.email.value = feedbackFormState.email;
   form.elements.message.value = feedbackFormState.message;
 
-  //   console.log('onFormUpdate->feedbackFormState :>> ', feedbackFormState);
+  // console.log('onFormUpdate->feedbackFormState :>> ', feedbackFormState);
 }
 
 function onInputChange() {
@@ -34,11 +41,17 @@ function onInputChange() {
 function onSubmit(e) {
   e.preventDefault();
   console.log('savedFormState :>> ', feedbackFormState);
+
+  // clean up form localStorage
   feedbackFormState.email = '';
   feedbackFormState.message = '';
   localStorage.setItem(
     'feedback-form-state',
     JSON.stringify(feedbackFormState)
   );
+  // clean up whole localStorage including vimeo player page
+  // localStorage.clear();
+  // ---------------------------------
+
   e.currentTarget.reset();
 }
